503a90129c5a3f5310e814b0e95906eae2c9cf40
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-02-12 13:58:20 +0100

    Simpler implem

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -4,14 +4,5 @@
 // The year is also evenly divisible by 400. Then it is a leap year.
 
 pub fn is_leap_year(year: i32) -> bool {
-    if year % 4 == 0 {
-        if year % 100 == 0 {
-            if year % 400 == 0 {
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-    return false;
+     year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
