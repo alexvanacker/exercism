93ba9c1b85ea566b3f4ea3675f0c101673013356
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-02-12 13:56:57 +0100

    First implem

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,17 @@
+// Leap year is defined by
+// The year can be evenly divided by 4;
+// If the year can be evenly divided by 100, it is NOT a leap year, unless;
+// The year is also evenly divisible by 400. Then it is a leap year.
+
 pub fn is_leap_year(year: i32) -> bool {
-    unimplemented!()
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            if year % 400 == 0 {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
