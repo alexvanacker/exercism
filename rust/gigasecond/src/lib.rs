7943a8392e4042b30fac3ffa8e4016a23dd522e1
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-02-12 13:36:19 +0100

    First solution proposal

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,5 +3,12 @@ use chrono::*;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!()
+    let billion = 1000000000;
+    let billion_duration = Duration::seconds(billion);
+    let date_time_plus_billion = start.checked_add_signed(billion_duration);
+    match date_time_plus_billion.is_some() {
+        true => return date_time_plus_billion.unwrap(),
+        false => panic!("Unsupported number of seconds"),
+    }
+   
 }
