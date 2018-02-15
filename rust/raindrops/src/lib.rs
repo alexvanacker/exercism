3561b3b74a2cba50fade69fb05542ec518bdfefb
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-02-15 14:11:52 +0100

    Raindrops: passes test V1

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!()
+    let mut result = String::from("");
+    if n.overflowing_rem(3) == (0, false) {
+        result.push_str("Pling");
+    }
+    if n.overflowing_rem(5) == (0, false) {
+        result.push_str("Plang");
+    }
+    if n.overflowing_rem(7) == (0, false) {
+        result.push_str("Plong");
+    }
+    if result.len() > 0 {
+        return result;
+    }
+    n.to_string()
 }
+
