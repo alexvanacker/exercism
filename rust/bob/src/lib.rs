3b08bef2a75a2ba1d22c0afcebe1b69266ce5556
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-02-26 13:21:58 +0100

    First working solution

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,38 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!()
+    println!("Original message '{}'", message);
+    let trimmed = message.trim();
+
+    if trimmed.len() == 0 {
+        return "Fine. Be that way!";
+    }
+
+    // Yelling = All Caps 
+    let uppercase = trimmed.to_uppercase();
+    let is_yelling = uppercase == trimmed && contains_letters(trimmed);
+
+    // Question = "?" at the end
+    let is_question = trimmed.chars().last() == Some('?');
+
+    if is_question && is_yelling {
+        return "Calm down, I know what I'm doing!";
+    }
+    if is_yelling {
+        return "Whoa, chill out!";
+    }
+
+    if is_question {
+        return "Sure.";
+    }
+
+    return "Whatever.";
+}
+
+pub fn contains_letters(message: &str) -> bool {
+
+    for character in message.chars() {
+        if character.is_alphabetic() {
+            return true;
+        }
+    }
+    false
 }
