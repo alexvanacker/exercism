ab4d76287ae3bdbca4f04c7cf92dbeb9859e5058
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-03-01 14:10:06 +0100

    Optimized solution
    Better sing function based on map + Vecs

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,12 +1,11 @@
 pub fn verse(n: i32) -> String {
-    let next_number = n - 1;
     let end_text = match n {
         0 => String::from("Go to the store and buy some more, 99 bottles of beer on the wall."),
         1 => String::from("Take it down and pass it around, no more bottles of beer on the wall."),
         2 => String::from("Take one down and pass it around, 1 bottle of beer on the wall."),
         _ => format!(
             "Take one down and pass it around, {} bottles of beer on the wall.",
-            next_number
+            n - 1
         ),
     };
 
@@ -25,12 +24,5 @@ pub fn verse(n: i32) -> String {
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    let mut result = String::new();
-    for i in (end..(start + 1)).rev() {
-        result.push_str(&verse(i));
-        if i > end {
-            result.push_str("\n");
-        }
-    }
-    result
+    (end .. start+1).rev().map(verse).collect::<Vec<_>>().join("\n")
 }
