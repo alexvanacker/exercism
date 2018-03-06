32dc515298e0136e69ca65b884bc822fe3c3b512
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-03-06 13:42:29 +0100

    Proverb: working solution

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!()
+    let len = list.len();
+    let mut proverb = String::from("");
+    if len == 0 {
+        return proverb;
+    }
+
+    for i in 0..len-1 {
+        proverb += &format!("For want of a {} the {} was lost.\n", list[i], list[i+1])
+    }
+    proverb += &format!("And all for the want of a {}.", list[0]);
+    proverb
 }
