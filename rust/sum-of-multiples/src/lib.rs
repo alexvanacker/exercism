a844a43d041fc28abf14c5617c43424af67f0345
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-03-07 13:28:03 +0100

    Sum multiples: working solution

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!()
+    (1..limit).filter(|x| is_multiple_of_factors(x, factors)).sum()
+}
+
+pub fn is_multiple_of_factors(n: &u32, factors: &[u32]) -> bool{
+    factors.iter().fold(false, |acc, x| acc || (n % x == 0))
 }
