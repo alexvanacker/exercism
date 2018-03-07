e0652208a56497acd45a01b56eecf32144d32828
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-03-07 13:56:38 +0100

    Grains: working solution

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!();
+    match s {
+        1...64 => 2u64.pow(s - 1),
+        _ => panic!("Square must be between 1 and 64"),
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..65).map(|x| square(x)).sum()
 }
