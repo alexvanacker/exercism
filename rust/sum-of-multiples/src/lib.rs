2009bb75c6627edc4d568c37dc5de758a455a2fb
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-03-07 13:37:42 +0100

    Sum multiples: use any for filtering
    One-liner power

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    (1..limit).filter(|x| is_multiple_of_factors(x, factors)).sum()
-}
-
-pub fn is_multiple_of_factors(n: &u32, factors: &[u32]) -> bool{
-    factors.iter().fold(false, |acc, x| acc || (n % x == 0))
+    (1..limit)
+        .filter(|x| factors.iter().any(|f| x % f == 0))
+        .sum()
 }
